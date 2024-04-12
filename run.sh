f260ac13db1a556b3f63ffbd3d32b92c8cb020d8
bofeng1997 <bofeng1997@gmail.com>
2024-04-13 04:24:23 +0800

    first commit.

---
+++ run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Set the C and C++ compilers
+export CMAKE_C_COMPILER=clang
+export CMAKE_CXX_COMPILER=clang++
+
+# Change to the project directory (replace with your actual project directory)
+cd ./build
+
+if [ -f toy ]; then
+  # If it exists, delete it
+  rm toy
+  echo "toy file deleted"
+else
+  # If it doesn't exist, print a message
+  echo "toy file not found"
+fi
+
+# Generate Ninja build files
+cmake -GNinja ..
+
+# Build the project
+ninja
+
+# Run the executable
+
+if [ -f toy ]; then
+  # If it exists, delete it
+   ./toy
+else
+  # If it doesn't exist, print a message
+  echo "compile error"
+fi
+
